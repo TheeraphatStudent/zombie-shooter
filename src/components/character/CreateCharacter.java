596e7a9d9a827b6e44b5d89be6bb9cf6d84c533a
TheeraphatCH <theeraphatchueanokkhum@gmail.com>
2024-10-07 22:27:26 +0700

    update: git ignore

--- src/components/character/CreateCharacter.java
+++ src/components/character/CreateCharacter.java
@@ -28,18 +28,6 @@ import page.home.GameCenter;
 import utils.LoadImage;
 import utils.UseText;
 
-interface ManageCharacterElement {
-    // Character
-    final int CHARACTER_WIDTH = 250;
-    final int CHARACTER_HEIGHT = 250;
-
-    // Weapon
-    final int WEAPON_WIDTH = 160;
-    final int WEAPON_HEIGHT = 155;
-    final double WEAPON_SCALE = 0.5;
-
-}
-
 interface CreateCharacterProps {
     // Method
     void setCharacterMoveLeft(boolean isMoveLeft);
